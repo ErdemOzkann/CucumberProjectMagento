5a927092bcade6bbc7c1602b26581d992b63b76b
Atilla Avci <atillatorosavci@hotmail.com>
2025-05-13 16:50:16 +0200

    TC_701 Register is done!

--- src/test/java/Pages/Elements.java
+++ src/test/java/Pages/Elements.java
@@ -1,10 +1,38 @@
 package Pages;
 
 import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Elements extends ParentPage {
     public Elements() {PageFactory.initElements(GWD.getDriver(),this);}
 
+    @FindBy(id = "accept-btn")
+    public WebElement acceptBtn;
+
+    @FindBy(xpath = "//*[text()='Create an Account']")
+    public WebElement createAnAccount;
+
+    @FindBy(id = "firstname")
+    public WebElement firstName;
+
+    @FindBy(id = "lastname")
+    public WebElement lastName;
+
+    @FindBy(id = "email_address")
+    public WebElement emailAddress;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(id = "password-confirmation")
+    public WebElement passwordConfirmation;
+
+    @FindBy(css = "[type='submit'][class='action submit primary']")
+    public WebElement createAccountButton;
+
+    @FindBy(xpath = "//*[text()='Thank you for registering with Main Website Store.']")
+    public WebElement registrationMsg;
 
 }
